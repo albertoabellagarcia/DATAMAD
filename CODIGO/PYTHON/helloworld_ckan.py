0053195b3ade12bcfbf76d60bb8d54d15c8cc0e0
Smart Data Models <alberto.abella@fiware.org>
2024-10-10 01:46:05 +0200

    primeros contenidos

---
+++ CODIGO/PYTHON/helloworld_ckan.py
@@ -0,0 +1,20 @@
+# Este programa es una ejemplo básico del uso del paquete ckanapi para la recuperación de datos de un portal basado en ckan como el de la comunidad de madrid
+
+from ckanapi import RemoteCKAN
+
+# Connect to the CKAN instance
+ckan = RemoteCKAN('https://datos.comunidad.madrid/api/')
+
+# Fetch a dataset (using the dataset ID or name)
+dataset = ckan.action.package_show(id='93bed3f0-3ba5-4b00-90bf-1c81951bab24')
+
+
+# Print dataset details
+print(dataset)
+
+# Search datasets
+result = ckan.action.package_search(q='aire')
+#
+# # Create or update a dataset (authenticated with an API key)
+# ckan_with_key = RemoteCKAN('https://your-ckan-instance-url.com', apikey='your-api-key')
+# ckan_with_key.action.package_create(name='new-dataset', title='New Dataset Title')
---
+++ EL PORTAL DE DATOS ABIERTOS DE LA COMUNIDAD DE MADRID/README.md
@@ -0,0 +1,4 @@
+
+
+El portal tiene un api que puede accederse en este enlace que nos devuelve un fichero json con la lista de los recursos disponibles paginado de 10 en 10 por defecto
+https://datos.comunidad.madrid/api/3/action/current_package_list_with_resources
